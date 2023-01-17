f0552d2f8dc4725987e277a0eb5a6170cbb74f9b
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-17 21:13:36 +0100

    remove database connection

--- chess-backend/src/main/java/de/chessy/Main.java
+++ chess-backend/src/main/java/de/chessy/Main.java
@@ -1,15 +1,12 @@
 package de.chessy;
 
-import de.chessy.database.DatabaseDriver;
 import de.chessy.server.ChessServer;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        DatabaseDriver.connect();
         ChessServer server = ChessServer.create(7999);
         server.start();
-        DatabaseDriver.closeConnection();
     }
 }
