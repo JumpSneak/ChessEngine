f04a7a8ba68728917f0f0538ba57e5046d6f60a9
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-20 00:21:58 +0100

    adjust ServerPiece

--- chess-client/core/src/com/jumpsneak/chessengine/transfer/responses/ServerPiece.java
+++ chess-client/core/src/com/jumpsneak/chessengine/transfer/responses/ServerPiece.java
@@ -1,4 +1,4 @@
 package com.jumpsneak.chessengine.transfer.responses;
 
-public record ServerPiece(int id, int belongsTo, int type) {
+public record ServerPiece(int id, boolean isWhite, int type) {
 }
