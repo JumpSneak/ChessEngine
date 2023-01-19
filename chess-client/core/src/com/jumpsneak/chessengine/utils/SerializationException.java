c3e3f20180b82313452d9178718389e0cc8ea2c9
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-20 00:08:01 +0100

    add serializer

---
+++ chess-client/core/src/com/jumpsneak/chessengine/utils/SerializationException.java
@@ -0,0 +1,7 @@
+package com.jumpsneak.chessengine.utils;
+
+public class SerializationException extends Exception {
+    public SerializationException(String message) {
+        super(message);
+    }
+}
