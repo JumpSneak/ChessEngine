708fb426b75b9795e6463fa2350066daab1326e2
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-19 21:01:52 +0100

    change eventKey to public

--- chess-backend/src/main/java/de/chessy/server/events/Event.java
+++ chess-backend/src/main/java/de/chessy/server/events/Event.java
@@ -1,7 +1,7 @@
 package de.chessy.server.events;
 
 public class Event {
-    private String eventKey;
+    public String eventKey;
 
     public Event(String eventKey) {
         this.eventKey = eventKey;
