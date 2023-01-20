70495d483ed16694b986e9902cc1c2206d926658
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-20 21:58:59 +0100

    make PieceMetadata serializable

--- chess-core/src/main/java/de/chessy/core/game/pieces/PieceMetadata.java
+++ chess-core/src/main/java/de/chessy/core/game/pieces/PieceMetadata.java
@@ -1,6 +1,7 @@
 package de.chessy.core.game.pieces;
 
-public abstract class PieceMetadata {
+// Note: cannot be abstract because it needs to be serializable
+public class PieceMetadata  {
 
     public final int id;
 
