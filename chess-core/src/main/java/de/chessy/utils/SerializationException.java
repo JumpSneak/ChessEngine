5e2a04398402a081d096f85b9603b3aff9d1394e
Julian Hartl <julian.hartl.dev@gmail.com>
2023-01-20 19:18:08 +0100

    add Serializer to core package

---
+++ chess-core/src/main/java/de/chessy/utils/SerializationException.java
@@ -0,0 +1,7 @@
+package de.chessy.utils;
+
+public class SerializationException extends Exception {
+    public SerializationException(String message) {
+        super(message);
+    }
+}
---
+++ chess-core/src/main/java/de/chessy/utils/Serializer.java
@@ -0,0 +1,42 @@
+package de.chessy.utils;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Serializer {
+    private Serializer() {
+
+    }
+
+    public static String serialize(Object object) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T fromJson(String body, Class<T> clazz) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(body, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static <T> T parse(InputStream body, Class<T> clazz) throws SerializationException {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(new InputStreamReader(body, StandardCharsets.UTF_8), clazz);
+        } catch (Exception e) {
+            throw new SerializationException(e.getMessage());
+        }
+    }
+}
